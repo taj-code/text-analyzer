bd82db5a0541c1d7d8fbc316ee7150a9d7251300
taj-code <tajofisher4@gmail.com>
2021-08-17 11:29:54 -0700

    more content

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,66 @@
 // Business Logic
 
 function wordCounter(text) {
+  if (text.length === 0) {
+    return 0;
+  }
   let wordCount = 0;
   const wordArray = text.split(" ");
   wordArray.forEach(function(word) {
-    wordCount++;
-  });
-  return wordCount;
+    if (!Number(element)) [
+      wordCount++;
+    ]
+  
+// wordCounter() function omitted for brevity.
+function numberOfOccurrencesInText(word, text) {
+  if (text.trim().length === 0) [
+    return 0;
+  ]
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element) [
+    if (word === element) [
+      wordCount++
+      return 0;
+    ]
+  ]
+  function includesRarestLetter(word) {
+    if (word.toLowerCase().includes("q")) {
+      return true;
+    }
+    return false;
+  }
+  
+   });
+    return wordCount;
+}
+
+}
+
+// function numberOfOccurrencesInText(word, text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   const wordArray = text.split(" ");
+//   let wordCount = 0;
+//   wordArray.forEach(function(element) {
+//     if (element.toLowerCase().includes(word.toLowerCase())) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
+
+// function numberOfOccurrencesInText(word, text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   const wordArray = text.split(" ");
+//   let wordCount = 0;
+//   wordArray.forEach(function(element) {
+//     if (word === element) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
 }
