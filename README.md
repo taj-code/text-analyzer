f8284a6456e3c07bc2a1f9ab4c00274771447ea8
taj-code <tajofisher4@gmail.com>
2021-08-17 15:37:23 -0700

    update

--- README.md
+++ README.md
@@ -57,4 +57,33 @@ const word = "Red";
 numberOfOccurrencesInText(word, text);
 Expected Output: 3
 
+Test: "If any empty string is passed in as a word, it should return 0."
+Code:
+const word = "";
+const text = "red RED Red!";
+wordCounter(word, text);
+Expected Output: 0
+
+Describe: boldPassage()
+
+Test: "It should return a non-matching word in a p tag."
+Code:
+const word = "hello";
+const text = "yo";
+boldPassage(word, text);
+Expected Output: "<p>yo</p>"
+
+Test: "It should return a matching word in a b tag."
+Code:
+const word = "hello";
+const text = "hello";
+boldPassage(word, text);
+Expected Output: "<p><b>hello</b></p>"
+
+Test: "It should wrap words that match in `b` tags but not words that don't."
+Code:
+const word = "hello";
+const text = "hello there";
+boldPassage(word, text);
+Expected Output: "<p><b>hello</b> there</p>"
 ```
--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,69 @@
+// Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
 // Business Logic
 
 function wordCounter(text) {
-  if (text.length === 0) {
+  if (text.trim().length === 0) {
     return 0;
   }
   let wordCount = 0;
   const wordArray = text.split(" ");
-  wordArray.forEach(function(word) {
-    if (!Number(element)) [
+  wordArray.forEach(function(element) {
+    if (!Number(element)) {
       wordCount++;
-    ]
-  
-// wordCounter() function omitted for brevity.
+    }
+  });
+  return wordCount;
+}
+
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) [
+  if (noInputtedWord(word, text)) {
     return 0;
-  ]
+  }
   const wordArray = text.split(" ");
   let wordCount = 0;
-  wordArray.forEach(function(element) [
-    if (word === element) [
-      wordCount++
-      return 0;
-    ]
-  ]
-  function includesRarestLetter(word) {
-    if (word.toLowerCase().includes("q")) {
-      return true;
+  wordArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
     }
-    return false;
+  });
+  return wordCount;
+}
+
+// UI Logic
+
+function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return "";
   }
-  
-   });
-    return wordCount;
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
 }
 
-}
-
-// function numberOfOccurrencesInText(word, text) {
-//   if (text.trim().length === 0) {
-//     return 0;
-//   }
-//   const wordArray = text.split(" ");
-//   let wordCount = 0;
-//   wordArray.forEach(function(element) {
-//     if (element.toLowerCase().includes(word.toLowerCase())) {
-//       wordCount++;
-//     }
-//   });
-//   return wordCount;
-// }
-
-// function numberOfOccurrencesInText(word, text) {
-//   if (text.trim().length === 0) {
-//     return 0;
-//   }
-//   const wordArray = text.split(" ");
-//   let wordCount = 0;
-//   wordArray.forEach(function(element) {
-//     if (word === element) {
-//       wordCount++;
-//     }
-//   });
-//   return wordCount;
-}
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
+  });
+});
